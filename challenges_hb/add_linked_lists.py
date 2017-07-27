7830b47437a7f30a958c15783f00882af89e532a
James Ye <jamye506@gmail.com>
2017-07-27 11:41:26 -0700

    methods added

--- challenges_hb/add_linked_lists.py
+++ challenges_hb/add_linked_lists.py
@@ -4,8 +4,9 @@
 
 class Node(object):
 
-    def __init__(self, data=None, next_node=None):
+    def __init__(self, data=None, next_node=None, head=None):
         self.data = data
+        self.head = head
         self.next_node = next_node
     
     def get_data(self):
@@ -14,20 +15,38 @@ class Node(object):
     def get_next(self):
         return self.next_node
 
+    def set_data(self, data):
+        self.data = data
+
     def set_next_node(self, new_next):
         self.next_node = new_next
 
+
 class Linked_list(object):
-    def __init__ (self, head=None):
+    def __init__(self, head=None):
         self.head = head
     
-    def insert(self, data):
+    def is_empty(self):
+        return self.head == None
+
+    def add(self, data):
         new_node = Node(data)
         new_node.set_next_node(self.head)
         self.head = new_node
 
+    def length(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.get_next()
 
-temp = Node(100)
 
-print(temp.get_data())
-
+my_list = Linked_list(12)
+my_list.add(31)
+my_list.add(77)
+my_list.add(17)
+my_list.add(93)
+my_list.add(26)
+my_list.add(54)
+print(my_list)
