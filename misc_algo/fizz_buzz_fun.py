a40e79d2a8ff4d21333a31667910e122f9fc84a2
James Ye <jamye506@gmail.com>
2017-09-08 18:57:07 -0700

    algorithm added

---
+++ misc_algo/fizz_buzz_fun.py
@@ -0,0 +1,12 @@
+def fizzy():
+    for i in range(101):
+        if i % 3 == 0 and i % 5 != 0:
+            print "fizz"
+        if i % 5 == 0 and i % 3 != 0:
+            print "buzz"
+        if i % 3 == 0 and i % 5 ==0:
+            print "fizzbuzz"
+
+
+
+fizzy()
