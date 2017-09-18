2304d5465a7132d773b1005e89603ed359784544
James Ye <jamye506@gmail.com>
2017-09-18 16:53:21 -0700

    algorithm added

--- misc_algo/queue.py
+++ misc_algo/queue.py
@@ -9,7 +9,31 @@ class Queue:
         self.items.insert(0, item)
 
     def dequeue(self):
-        return self.items.pop()
+          return self.items.pop()
 
     def size(self):
         return len(self.items)
+
+
+
+#simple queue game of hot potatoe
+
+def hot_potatoe(name_list,num):
+    sim_queue = Queue()
+    for name in name_list:
+        sim_queue.enqueue(name)
+
+    while sim_queue.size() > 1:
+        for i in range(num):
+            sim_queue.enqueue(sim_queue.dequeue())
+
+        sim_queue.dequeue()
+    return sim_queue.dequeue()
+
+print(hot_potatoe(["fail","win","w/e","lost","test"],19))
+
+q = Queue()
+q.enqueue('hello')
+q.enqueue('dog')
+q.enqueue(3)
+q.dequeue()
