33a80b241dd6616bf3b13f0a710afaf6ac17a733
James Ye <jamye506@gmail.com>
2017-10-25 13:26:26 -0700

    Updated algorithm

--- prep/check_permutations.py
+++ prep/check_permutations.py
@@ -5,15 +5,30 @@ CTCI Page 90
 Given two strings, write a method to decide if one is a permutation of the other
 
 """
+#This solution on the bottom only applies to strings that are the same length
+def characters_check(some_str):
+    new_dict = {}
+
+    for i in some_str:
+        if new_dict.has_key(i) is False:
+            new_dict[i] = 1
+        elif new_dict.has_key(i) is True:
+            new_dict[i] = new_dict[i] + 1
+
+    return new_dict
 
 def permutation_checker(str_one, str_two):
-    letter_count = {}
 
-    for i in str_one:
-        if letter_count.has_key(i) is False:
-            letter_count[i] = 1
-        elif letter_count.has_key(i) is True:
-            letter_count[i] = letter_count[i] + 1
-    print letter_count
+    str_one_dict = characters_check(str_one)
+    str_two_dict = characters_check(str_two)
 
-permutation_checker("hello","world")    
+    if str_one_dict == str_two_dict:
+        print ("%s and %s are permutations" % (str_one, str_two))
+    else:
+        print ("Not permutations")
+
+
+permutation_checker("god","ogd")
+permutation_checker("hello","world")
+permutation_checker("taco","coat")
+permutation_checker("harry","potter")
